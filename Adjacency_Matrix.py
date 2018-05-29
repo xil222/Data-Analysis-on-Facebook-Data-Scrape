4c11036e0c8be9d659a1d8e7c4a2333d81e26dfd
Joy <supergaljoy1994@gmail.com>
2018-05-28 18:46:34 -0700

    created dataframe
    tried to view dataframe then computer crashed. Check other examples to see if networkx is just slow

--- Adjacency_Matrix.py
+++ Adjacency_Matrix.py
@@ -9,9 +9,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+from scipy.sparse import find
 
+global school_names
 
-def get_Adjacency_Matrices():
+def get_Data():
     '''
     TODO - change this docstring
     Extracts the data of all the mat files in the data directory.
@@ -20,22 +22,51 @@ def get_Adjacency_Matrices():
     :return: school_second_major
     :type: numpy array
     '''
+    global school_names
     schools = scipy.io.loadmat('schools.mat') #load the mat file with the schools in order
-    school_data = [] #initialize array
+    school_data = {} #initialize dictionary to contain name with sparse matrix
+    school_names= {} #dictionary of name? --- TODO - Remove if not needed ----
+    #parse each entry
     for num,i in enumerate(schools['schools'],1): #iterate through the school names arrays
         fnamestring =  np.array_str(i[0]) #get the school name from the array ex: [u'Penn'] brackets included
         fname = fnamestring[3:-2] #slice the string to just get the only the name of the school
-        school_data.append(scipy.io.loadmat(fname + str(num))['A']) #append the matfile to the array
+        school_data[fname + str(num)]=(scipy.io.loadmat(fname + str(num))['A']) #append the matfile to the array
+        #school_names[fname] = num
+    #print school_data
+    #school_data = np.asarray((school_data))
+    #print np.shape(school_data[99])
+    #(rows, col) = np.shape(school_data[99])
+    #print rows
+    #print school_data[99].todense()
+    #print school_data[99][1,:]
+    #print school_data[99][1,:].toarray()
+    #print school_data[99][1,2]
+    return school_data
 
-    school_data = np.asarray((school_data))
-    print np.shape(school_data[0])
+def get_School_Adj_Matrix(school_data, name):
+    '''
+    Adjacency lists are in compressed sparse column format
+    '''
+    return school_data[name]#order of 1-100 but index of 1-99
+
+def create_Network(school_Mat):
     return
-
 
 
 def main():
     plt.close('all')
-    get_Adjacency_Matrices()
+    school_data = get_Data()
+    (rows,col,values) = find(school_data['UIllinois20']) # find returns nonzero rows/columns/values
+    #print (school_data['UIllinois20'])
+    #print rows
+    df = pd.DataFrame({'from': rows, 'to': col})
+    G=nx.from_pandas_edgelist(df, 'from', 'to')
+ 
+    # Plot the network:
+    nx.draw(G, with_labels=True, node_color='orange', node_size=400, edge_color='black', linewidths=1, font_size=15)
 
+    #for University in school_data: #for each university (key) in school_data
+      #  school_Adj = school_data[University]
+    
 
 main()
