d5a1a591114ee9846e3343e8cea2e7a6e8c76a91
Joy <supergaljoy1994@gmail.com>
2018-05-29 18:20:01 -0700

    each radio button creates a new figure window

--- Adjacency_Matrix.py
+++ Adjacency_Matrix.py
@@ -2,7 +2,9 @@
 """
 Created on Sun May 27 00:15:29 2018
 
-@author: Super
+@author: Joy Nwarueze
+
+
 """
 import scipy.io
 import numpy as np
@@ -10,7 +12,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 from scipy.sparse import find
-global school_names
 from matplotlib.widgets import RadioButtons
 
 global school_dict
@@ -19,45 +20,37 @@ def get_Data():
     '''
     TODO - change this docstring
     Extracts the data of all the mat files in the data directory.
-    Then extracts the second major column from all the schools into a 2D matrix
-    with the row showing the data and each row is a different school
-    :return: school_second_major
-    :type: numpy array
+    Then stores the adjacency matrix of each mat file into a dict of {University: matrix}
+    key and value pairs
+    @return school_data: dictionary - A dictionary with University+num as key and Adjacency matrix
+    as the value
     '''
-    global school_names
     schools = scipy.io.loadmat('schools.mat') #load the mat file with the schools in order
     school_data = {} #initialize dictionary to contain name with sparse matrix
-    school_names= {} #dictionary of name? --- TODO - Remove if not needed ----
+    
     #parse each entry
     for num,i in enumerate(schools['schools'],1): #iterate through the school names arrays
         fnamestring =  np.array_str(i[0]) #get the school name from the array ex: [u'Penn'] brackets included
         fname = fnamestring[3:-2] #slice the string to just get the only the name of the school
         school_data[fname + str(num)]=(scipy.io.loadmat(fname + str(num))['A']) #append the matfile to the array
-        #school_names[fname] = num
-    #print school_data
-    #school_data = np.asarray((school_data))
-    #print np.shape(school_data[99])
-    #(rows, col) = np.shape(school_data[99])
-    #print rows
-    #print school_data[99].todense()
-    #print school_data[99][1,:]
-    #print school_data[99][1,:].toarray()
-    #print school_data[99][1,2]
     return school_data
 
-def get_School_Adj_Matrix(school_data, name):
-    '''
-    Adjacency lists are in compressed sparse column format
-    '''
-    return school_data[name]#order of 1-100 but index of 1-99
 
 def store_School_With_Graph(school_data):
+    '''
+    Creates a small graph for each University then store it in a dictionary with University as key
+    and Graph as the value
+    
+    @param school_data: dictionary - A dictionary with University+num as key and Adjacency matrix
+    as the value
+    @return school_dict: dictionary - A dictionary with University+num as key and Graph as the value
+    '''
     global school_dict
     school_dict = {}
     for name in school_data:
-        (rows,col,values) = find(school_data['UIllinois20']) # find returns nonzero rows/columns/values
-        short_Rows = rows[:len(rows)/1000]
-        short_Cols = col[:len(col)/1000]
+        (rows,col,values) = find(school_data[name]) # find returns nonzero rows/columns/values
+        short_Rows = rows[:len(rows)/5000]
+        short_Cols = col[:len(col)/5000]
         df = pd.DataFrame({'from': short_Rows, 'to': short_Cols})
         G=nx.from_pandas_edgelist(df, 'from', 'to')
         degree_dict = dict(G.degree(G.nodes()))
@@ -66,21 +59,21 @@ def store_School_With_Graph(school_data):
     return school_dict
 
 
-
-
 def main():
+    '''
+    Calls the method to extract the data from .mat files, as well as create University+AdjMatrix and University+Graph dictionaries
+    Also draws the starting University
+    '''
     global school_dict 
     school_data = get_Data()
     school_dict = store_School_With_Graph(school_data) #contains graph of each school by name
-
-    assert school_dict.has_key('UIllinois20')
-    G = school_dict['UIllinois20']
-    degree_dict = nx.get_node_attributes(G, 'degree')   
-    nx.draw(G,ax = ax2 ,with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.fruchterman_reingold_layout(G))
+    #G = school_dict['UIllinois20']
+    #degree_dict = nx.get_node_attributes(G, 'degree')   
+    #nx.draw(G,ax = ax2 ,with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.fruchterman_reingold_layout(G))
     return
 
 plt.close('all')    
-fig = plt.figure(figsize=(8,8))
+fig = plt.figure(figsize=(10,10))
 fig.set_facecolor("#00000F")
 axis = plt.axis(facecolor='springgreen')
 fig.add_axes(axis)
@@ -89,24 +82,103 @@ ax2= axes_list[0]
 main()
 
 
-axcolor = 'skyblue'
-rax = plt.axes([0.05, 0.7, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ('UIllinois20', 'Penn94'))
-#radio = RadioButtons(rax, tuple(school_dict.keys()))
+axcolor = 'cyan'
+first33pairs = {k: school_dict[k] for k in school_dict.keys()[:33]}
+next33pairs = {k: school_dict[k] for k in school_dict.keys()[33:67]}
+last33pairs = {k: school_dict[k] for k in school_dict.keys()[67:]}
+#[0.05, 0.7, 0.15, 0.15] -> [left, bottom, width, height]
+#bot left
+rax = plt.axes([0.01, 0.1, 0.15, 0.85], facecolor=axcolor)
+radio = RadioButtons(rax, tuple(first33pairs.keys()))
+
+#bot right
+rax = plt.axes([0.8, 0.1, 0.15, 0.85], facecolor=axcolor)
+radio2 = RadioButtons(rax, tuple(next33pairs.keys()))
+
+#bot middle
+rax = plt.axes([0.4, 0.1, 0.15, 0.85], facecolor=axcolor)
+radio3 = RadioButtons(rax, tuple(last33pairs.keys()))
+
 plt.sca(ax2)
-plt.show()
+
 
 def network(label):
+    '''
+    Draws the network of the selected University
+    
+    @param label: string - The name of the radio button for the University selected
+    '''
     global school_dict
-    plt.cla()
-    plt.sca(ax2)
-    #axes_list = fig.get_axes()
-    #print axes_list
-    #print fig.axes
+    #plt.cla() 
+    
+    fig2 = plt.figure(2,figsize=(10,10))
+    fig2.clear()
+    
     G = school_dict[label]
     degree_dict = nx.get_node_attributes(G, 'degree')
-    #Notes : fruchterman_reingold_layout(G) - layout based on larger hubs near center 
-    nx.draw(G, ax=ax2, with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.fruchterman_reingold_layout(G))
-    fig.set_facecolor("#00000F")
-    plt.show()
+    #Notes : fruchterman_reingold_layout(G) - layout based on larger hubs near center (force graph)
+    nx.draw(G, with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.fruchterman_reingold_layout(G))
+    
+    fig2.set_facecolor("#00000F")
+    plt.draw()
 radio.on_clicked(network)
+
+
+def network2(label):
+    '''
+    Draws the network of the selected University
+    
+    @param label: string - The name of the radio button for the University selected
+    '''
+    global school_dict
+    #plt.cla()
+    #plt.sca(ax2)
+    
+    fig3 = plt.figure(3, figsize= (10,10))
+    fig3.clear()
+    
+    G = school_dict[label]
+    degree_dict = nx.get_node_attributes(G, 'degree')
+    nx.draw(G, with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.spring_layout(G))
+    
+    fig3.set_facecolor("#00000F")
+    plt.draw()
+radio2.on_clicked(network2)
+
+
+def network3(label):
+    '''
+    Draws the network of the selected University
+    
+    @param label: string - The name of the radio button for the University selected
+    '''
+    global school_dict
+    #plt.cla()
+    #plt.sca(ax2)
+    
+    fig4 = plt.figure(4, figsize= (10,10))
+    fig4.clear()
+    
+    G = school_dict[label]
+    degree_dict = nx.get_node_attributes(G, 'degree')
+    nx.draw(G, with_labels=False, node_color='darkviolet', node_size=[size*10 for size in degree_dict.values()], edge_color='red', alpha = 0.5, linewidths=2, font_size=5,pos=nx.spring_layout(G))
+    
+    fig4.set_facecolor("#00000F")
+    plt.draw()
+radio3.on_clicked(network3)
+
+
+
+
+'''
+list of layouts
+pos=nx.fruchterman_reingold_layout(G) - default layout based on force graph
+pos=nx.circular_layout(G) - pretty good showing of density UCSD is like 10 lines lol though its right side oriented... +
+pos=nx.random_layout(G) - Kinda doesnt tell too much, just that more connections means more lines crossing each other (at random)... X
+pos=nx.spectral_layout(G) - i dont even know what it's meant to show... X
+pos=nx.spring_layout(G) - looks exactly the same as the F_R layout tbh so its fine
+'''
+
+
+
+plt.show()
